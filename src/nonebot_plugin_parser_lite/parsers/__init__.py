445244a5b642c869c69951d9a260254d9f253a99
molanp <104612722+molanp@users.noreply.github.com>
2026-06-26 20:25:37 +0800

    refactor(parsers/toutiao): 移除今日头条解析支持

--- src/nonebot_plugin_parser_lite/parsers/__init__.py
+++ src/nonebot_plugin_parser_lite/parsers/__init__.py
@@ -1,15 +1,4 @@
-from ..creator import Creator
-from ..data import (
-    AudioContent,
-    Author,
-    GraphicContent,
-    ImageContent,
-    ParseResult,
-    Platform,
-    VideoContent,
-)
 from .acfun import AcfunParser
-from .base import BaseParser, handle
 from .bilibili import BilibiliParser
 from .buff import BuffParser
 from .coolapk import CoolapkParser
@@ -25,40 +14,28 @@ from .netease import NCMParser
 from .qsmusic import QSMusicParser
 from .rednote import RedNoteParser
 from .tieba import TiebaParser
-from .toutiao import ToutiaoParser
 from .weibo import WeiBoParser
 from .x import XParser
 from .zhihu import ZhiHuParser
 
 __all__ = [
     "AcfunParser",
-    "AudioContent",
-    "Author",
-    "BaseParser",
     "BilibiliParser",
     "BuffParser",
     "CoolapkParser",
-    "Creator",
     "DouyinParser",
     "DuiTangParser",
-    "GraphicContent",
     "HeyBoxParser",
     "IlluParser",
-    "ImageContent",
     "KuGouParser",
     "KuWoParser",
     "KuaiShouParser",
     "LofterParser",
     "NCMParser",
-    "ParseResult",
-    "Platform",
     "QSMusicParser",
     "RedNoteParser",
     "TiebaParser",
-    "ToutiaoParser",
-    "VideoContent",
     "WeiBoParser",
     "XParser",
     "ZhiHuParser",
-    "handle",
 ]
