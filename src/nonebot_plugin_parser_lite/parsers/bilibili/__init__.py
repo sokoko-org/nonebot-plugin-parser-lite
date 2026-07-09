6dcff7e2905851e2573233cf526c818ec4112d2e
molanp <104612722+molanp@users.noreply.github.com>
2026-07-09 21:38:59 +0800

    fix(bilibili): 修复哔哩哔哩下载缓存功能
    为Bilibili视频下载功能添加正确的.mp4扩展名，
    确保下载的视频文件具有正确的文件格式后缀。

--- src/nonebot_plugin_parser_lite/parsers/bilibili/__init__.py
+++ src/nonebot_plugin_parser_lite/parsers/bilibili/__init__.py
@@ -334,7 +334,7 @@ class BilibiliParser(BaseParser):
                     return await DOWNLOADER.download_av_and_merge(
                         self.video_url,
                         self._audio_url,
-                        file_name=file_base,
+                        file_name=f"{file_base}.mp4",
                         ext_headers=self.ext_headers,
                     )
                 # 否则直接用流式下载
