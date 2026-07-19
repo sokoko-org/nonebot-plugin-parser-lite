38dab1ac6d6f33070528ad1ec5c083760d5b1455
molanp <104612722+molanp@users.noreply.github.com>
2026-07-19 21:14:51 +0800

    chore(version): 升级插件版本号到1.3.0
    更新 __plugin_meta__ 中的版本号从 1.2.9 到 1.3.0，
    以反映新版本的发布。

--- src/nonebot_plugin_parser_lite/__init__.py
+++ src/nonebot_plugin_parser_lite/__init__.py
@@ -27,7 +27,7 @@ __plugin_meta__ = PluginMetadata(
     extra={
         "author": "molanp",
         "homepage": "https://github.com/sokoko-org/nonebot-plugin-parser-lite",
-        "version": "1.2.9",
+        "version": "1.3.0",
         "plugin_type": "NORMAL",
     },
 )
