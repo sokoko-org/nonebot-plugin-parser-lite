6a33f12f7e0cebded2ea081cee6de6c803b5d88d
molanp <104612722+molanp@users.noreply.github.com>
2026-09-13 00:38:36 +0800

    fix(render): 修复渲染图底部的意外空白

--- src/nonebot_plugin_parser_lite/__init__.py
+++ src/nonebot_plugin_parser_lite/__init__.py
@@ -26,7 +26,7 @@ __plugin_meta__ = PluginMetadata(
     extra={
         "author": "sokoko-org",
         "homepage": "https://github.com/sokoko-org/nonebot-plugin-parser-lite",
-        "version": "1.3.7-pre-release.2",
+        "version": "1.3.7-pre-release.3",
         "plugin_type": "NORMAL",
     },
 )
